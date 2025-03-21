70ab1f2be44e368b6e165566d3c8d9c733fb2ff0
Nguyen Minh Triet Luu <luunguyenminhtriet021025@gmail.com>
2025-03-21 15:20:04 -0400

    Update for lab11

---
+++ Week11/Person.py
@@ -0,0 +1,20 @@
+class Person():
+    def __init__(self, p_name, p_age, p_height):
+        self.name = p_name
+        self.age = p_age
+        self.height = p_height
+        self.public_prop = "I am public property"
+
+        @property
+        def name(self):
+            return self._name
+        @name.setter
+        def name(self, new_name):
+            self._name = new_name
+
+        def __del__(self):
+            print("The garbage collector is automatically destroying person objects")
+
+
+person1 = Person("John", 25, 5.8)
+print("The name of the person is: ", person1.name)
